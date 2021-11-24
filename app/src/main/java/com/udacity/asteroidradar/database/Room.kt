7d72df0c3c023c890921ec484d3ab632ee5566cd
AHMED Rabee <35776251+Ahmed7313@users.noreply.github.com>
2021-11-25 01:21:46 +0200

    asf

---
+++ app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -0,0 +1,33 @@
+package com.udacity.asteroidradar.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface AsteroidDao {
+
+    @Query("SELECT * FROM databaseasteroid")
+    fun getAsteroids() : LiveData<List<DatabaseAsteroid>>
+
+    @Insert
+    fun insertAllAsteroids(vararg asteroid: DatabaseAsteroid)
+}
+
+@Database(entities = [AsteroidDatabase::class], version = 1)
+abstract class AsteroidDatabase : RoomDatabase(){
+    abstract val asteroidDao: AsteroidDao
+}
+
+private lateinit var INSTANCE: AsteroidDatabase
+
+fun getDatabase(context: Context): AsteroidDatabase {
+    synchronized(AsteroidDatabase::class.java){
+    if (!::INSTANCE.isInitialized){
+        INSTANCE = Room.databaseBuilder(context.applicationContext,
+            AsteroidDatabase::class.java,
+            "Asteroids").build()
+    }
+    }
+    return INSTANCE
+}
