72614526c5b2964d4dffda2ade866093849ab584
AHMED Rabee <35776251+Ahmed7313@users.noreply.github.com>
2021-11-24 21:19:40 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiServiece.kt
@@ -0,0 +1,39 @@
+package com.udacity.asteroidradar.api
+
+import androidx.lifecycle.LiveData
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface AsteroidApiService {
+
+    @GET("neo/rest/v1/feed")
+    fun getAsteroids(
+        @Query("start_date") start_date: String,
+        @Query("end_date") end_date: String,
+        @Query("api_key") api_key: String = Constants.API_KEY
+    ): Deferred<LiveData<List<Asteroid>>>
+}
+
+
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+object Network {
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(Constants.BASE_URL)
+        .build()
+
+    val service: AsteroidApiService = retrofit.create(AsteroidApiService::class.java)
+}
